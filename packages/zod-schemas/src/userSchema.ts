d6ee9b0533a38da0e23220e9e1e96df66634864e
krishan kumar safi <krishankumarsafi7@gmail.com>
2025-01-17 18:37:06 +0530

    add-signin & signup

--- packages/zod-schemas/src/userSchema.ts
+++ packages/zod-schemas/src/userSchema.ts
@@ -3,6 +3,8 @@ import z from "zod";
 export const userSchema = z.object({
   email: z.string(),
   password: z.string(),
+  name: z.string(),
+  phoneNumber: z.number(),
 });
 
 // export type USER = z.infer<typeof userSchema>;
