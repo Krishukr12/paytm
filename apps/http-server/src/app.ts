fc1a08aa0cb0486994151fff03b9886e0410b64c
krishan kumar safi <krishankumarsafi7@gmail.com>
2025-02-11 13:28:55 +0530

    add-welcome

--- apps/http-server/src/app.ts
+++ apps/http-server/src/app.ts
@@ -12,6 +12,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("welcome to the bank");
+});
+
 // ROUTES
 //User Routes
 app.use("/api/v1/user", userRouter);
