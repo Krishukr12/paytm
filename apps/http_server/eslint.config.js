612c7247f385e76cbd7f6bc87540e34160a897f4
krishan kumar safi <krishankumarsafi7@gmail.com>
2025-02-12 18:51:25 +0530

    fix-erro

---
+++ apps/http_server/eslint.config.js
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+import typescript from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["src/**/*.{js,ts}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": typescript,
+    },
+    rules: {
+      ...typescript.configs.recommended.rules,
+    },
+  },
+]; 
